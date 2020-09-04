16e24c5809e52c5069175aed8c2c8d4a4ad6b488
Aditya Khandelwal <aditya.khandelwal@bytedance.com>
2020-09-04 11:12:57 -0700

    Created empty project with test app

---
+++ TikTokBusinessSDK/TikTokBusinessSDK.h
@@ -0,0 +1,19 @@
+//
+//  TikTokBusinessSDK.h
+//  TikTokBusinessSDK
+//
+//  Created by Aditya Khandelwal on 9/4/20.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TikTokBusinessSDK.
+FOUNDATION_EXPORT double TikTokBusinessSDKVersionNumber;
+
+//! Project version string for TikTokBusinessSDK.
+FOUNDATION_EXPORT const unsigned char TikTokBusinessSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TikTokBusinessSDK/PublicHeader.h>
+
+
---
+++ TikTokBusinessSDKTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTokBusinessSDKTestApp
+//
+//  Created by Aditya Khandelwal on 9/4/20.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
