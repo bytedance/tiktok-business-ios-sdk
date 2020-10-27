d208b5264e9e13a1e192bf549b904ebcb7b1a869
Aditya Khandelwal <aditya.khandelwal@bytedance.com>
2020-10-27 13:59:02 -0700

    commented out logging request bodies in request handler

--- TikTokBusinessSDK/TikTokRequestHandler.m
+++ TikTokBusinessSDK/TikTokRequestHandler.m
@@ -158,9 +158,9 @@
     NSString *postLength = [NSString stringWithFormat:@"%lu", [postData length]];
     
     // TODO: Logs below to view JSON passed to request. Remove once convert to prod API
-     NSString *postDataJSONString = [[NSString alloc] initWithData:postData encoding:NSUTF8StringEncoding];
-     [self.logger info:@"[TikTokRequestHandler] Access token: %@", [[TikTok getInstance] accessToken]];
-     [self.logger info:@"[TikTokRequestHandler] postDataJSON: %@", postDataJSONString];
+//     NSString *postDataJSONString = [[NSString alloc] initWithData:postData encoding:NSUTF8StringEncoding];
+//     [self.logger info:@"[TikTokRequestHandler] Access token: %@", [[TikTok getInstance] accessToken]];
+//     [self.logger info:@"[TikTokRequestHandler] postDataJSON: %@", postDataJSONString];
     
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     NSString *url = [NSString stringWithFormat:@"%@%@%@", @"https://ads.tiktok.com/open_api/", self.apiVersion == nil ? @"v1.1" : self.apiVersion, @"/app/batch/"];;
